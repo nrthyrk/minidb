aecd2a44546abb322f9821d37c4d179922f8e7ff
cp_shen <cp_shen@outlook.com>
2018-11-18 15:19:56 +0800

    change to clang and add example without readline

--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
-CC = g++
-# CC = clang++
+# CC = g++
+CC = clang++
 # CC_FLAGS = -Wall -Werror -g
 CC_FLAGS = -g
-SRC_CPP = $(shell find src/ -name '*.cc')
-CC_LINK = -lreadline -lboost_filesystem -lboost_serialization -lboost_system -lboost_regex
+SRC_CC = $(shell find src/ -name '*.cc')
+MAIN = src/main.cpp
+EXAMPLE = src/example.cpp
+BOOST_LINK = -lboost_filesystem -lboost_serialization -lboost_system -lboost_regex
 BUILD = bin/minidb
+BUILD_EG = bin/example
 
-all:
-	$(CC) $(CC_FLAGS) $(SRC_CPP) $(CC_LINK) -o $(BUILD)
+executable:
+	$(CC) $(CC_FLAGS) $(SRC_CC) $(MAIN) $(BOOST_LINK) -lreadline -o $(BUILD)
+
+example:
+	$(CC) $(CC_FLAGS) $(SRC_CC) $(EXAMPLE) $(BOOST_LINK) -o $(BUILD_EG)
---
+++ src/example.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <boost/algorithm/string.hpp>
+
+#include "interpreter.h"
+
+
+int main(int argc, const char* argv[]) {
+
+    // output help info
+    std::cout << "usage: example <sql>;" << std::endl;
+    // check input
+    if(argc != 2){
+        return 1;
+    }
+
+    Interpreter itp;
+    const char *line = argv[1];
+    std::string sql = std::string(line);
+    boost::algorithm::trim(sql);
+    
+    if (sql.compare(0, 4, "exit") == 0 || sql.compare(0, 4, "quit") == 0) {
+        itp.ExecSQL("quit");
+    }
+    
+    itp.ExecSQL(sql);
+    std::cout << std::endl;
+
+    return 0;
+}
